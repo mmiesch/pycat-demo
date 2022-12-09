31c79d96b87e9357f43a95809db035f5df85b695
mmiesch <mark.s.miesch@gmail.com>
2022-12-09 11:33:34 -0700

    add a title

--- app.py
+++ app.py
@@ -2,9 +2,23 @@ from dash import Dash, dcc, html
 
 app = Dash(__name__)
 
-slider = dcc.Slider(min=1, max=10, step=1, value=1)
+slider = html.Div(
+  children=[
+    html.Label("Frame selection", htmlFor="frame-slider"),
+    dcc.Slider(min=1, max=10, step=1, value=1, id="frame-slider")
+  ]
+)
 
-app.layout = html.Div(slider, style={"margin": 30})
+app.layout = html.Div(
+    children=[
+      html.H1(children="Dash Demo",),
+      html.P(
+        children="Demo of Plotly DASH relevant to pyCAT",
+      ),
+      slider
+    ],
+    style={"margin": 30}
+)
 
 if __name__ == "__main__":
     app.run(debug=True)
