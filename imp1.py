bc565f4394329c6ff7739a20fc3ba0b3f3dec89f
mmiesch <mark.s.miesch@gmail.com>
2023-02-26 11:11:07 -0700

    remove scatterplot

--- imp1.py
+++ imp1.py
@@ -10,7 +10,6 @@ from skimage.measure import block_reduce
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import json
 import sunpy.visualization.colormaps as cm
 
@@ -22,13 +21,6 @@ import plotly.express as px
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
-
-#------------------------------------------------------------------------------
-# scatterplot data
-
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
-
-available_countries = df['country'].unique()
 
 #------------------------------------------------------------------------------
 # image data
@@ -85,28 +77,15 @@ fig.update_layout({
         "visible": False
     },
     "showlegend": False,
-    "height": 800,
+    "height": 600,
     "paper_bgcolor": "black",
     "plot_bgcolor": "black",
 })
 
 #------------------------------------------------------------------------------
+# define layout
 
 app.layout = html.Div([
-    dcc.Graph(
-        id='scatterplot-graph'
-    ),
-    dcc.Store(
-        id='scatterplot-figure-store'
-    ),
-    'Country',
-    dcc.Dropdown(available_countries, 'Canada', id='scatterplot-country-slider'),
-    'Graph scale',
-    dcc.RadioItems(
-        ['linear', 'log'],
-        'linear',
-        id='scatterplot-log-button'
-    ),
     dcc.Graph(
         id='graph'
     ),
@@ -129,37 +108,6 @@ app.layout = html.Div([
         )
     ])
 ])
-
-
-@app.callback(
-    Output('scatterplot-figure-store', 'data'),
-    Input('scatterplot-country-slider', 'value')
-)
-def update_store_data(country):
-    dff = df[df['country'] == country]
-    return px.scatter(dff, x='year', y='pop')
-
-app.clientside_callback(
-    """
-    function(figure, scale) {
-        if(figure === undefined) {
-            return {'data': [], 'layout': {}};
-        }
-        const fig = Object.assign({}, figure, {
-            'layout': {
-                ...figure.layout,
-                'yaxis': {
-                    ...figure.layout.yaxis, type: scale
-                }
-             }
-        });
-        return fig;
-    }
-    """,
-    Output('scatterplot-graph', 'figure'),
-    Input('scatterplot-figure-store', 'data'),
-    Input('scatterplot-log-button', 'value')
-)
 
 app.clientside_callback(
     """
